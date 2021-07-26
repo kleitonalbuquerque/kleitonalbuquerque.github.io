e3296e2080a4652dd6651ad8d02c6fca28d25a41
Kleiton Bezerra <kleiton2102@gmail.com>
2021-07-26 11:38:07 -0300

    Add github and linkedin

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 function reloadPage() {
-  console.log('E-mail enviado com sucesso');
-  setTimeout(() => {
-    window.location.reload();
-  }, 5000);
+  var inName    = document.getElementById("name");
+  var inEmail   = document.getElementById("email");
+  var inMessage = document.getElementById("message");
+
+  var name    = inName.value;
+  var email   = inEmail.value;
+  var message = inMessage.value;
+
+  if(name == "" || email == "" || message == "") {
+    alert("Preencha todos os campos =)")
+    inName.focus();
+    return
+  } else {
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+    console.log('E-mail enviado com sucesso');
+  }
 }
+
+var send = document.getElementById("send")
+send.addEventListener('click', reloadPage)
